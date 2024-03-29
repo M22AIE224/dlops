32db27f1242dab73ff477b9524cb3a128dd19e5e
Sharma <Prabha_Sharma@DELL.com>
2024-03-29 23:34:56 +0530

    fixed the issue

--- activation_function.py
+++ activation_function.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #print(np.linspace(-5, 5, 100))
 
-random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+random_values = np.array([-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6])
 print("Original Input Data - ", np.array(random_values))
 
 #def getSigmoid(input_val):
